66301c473ff62b165240d88ad0827c1e1a7b6072
Remi Nyzell Tonning <remitonning@gmail.com>
2013-03-05 21:42:25 +0100

    Added EmployeeDB.cs

---
+++ App_Code/EmployeeDB.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for EmployeeDB
+/// </summary>
+public class EmployeeDB
+{
+	public EmployeeDB()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+    internal void AddLog(Log log)
+    {
+
+    }
+    internal void AddSickDays(User user)
+    {
+
+    }
+    internal void AddChildSickDays(User user)
+    {
+
+    }
+}
